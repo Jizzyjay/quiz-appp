580b5d0f4d7ca54a8fa1245d230141394b2101e9
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2023-01-22 22:50:18 +0100

    client branch

--- server/database/conn.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export default async function connect(){
-    await mongoose.connect(process.env.ATLAS_URI)
-        // mongoose.set('strictQuery', false);
-    console.log("Database Connected")
-}
